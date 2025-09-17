90cf20e0752e4b510b7f7874ffd205437dea9166
uma-oo <oumayma.elfahsi99@gmail.com>
2025-09-17 13:12:42 +0100

    may be the latest version

--- src/utils.rs
+++ src/utils.rs
@@ -15,9 +15,19 @@ pub fn draw_lines_points(points: Vec<Point>) -> Vec<(Point, Point)> {
 }
 
 // each times we need the current points
-pub fn Chaikin_iteration(points: Vec<(Point, Point)>) -> Vec<Point> {
+pub fn chaikin_iteration(points: Vec<(Point, Point)>) -> Vec<Point> {
     let mut lines_iteration = Vec::new();
-    for (p1, p2) in points {
+
+    let (start, _) = points[0];
+    let (_, end) = points[points.len() - 1];
+    lines_iteration.push(start);
+    let mut i = 0;
+    for (p1, p2) in &points {
+        if i == 0 {
+            let (start, _) = points[0];
+            lines_iteration.push(start);
+        }
+
         let point_q = Point(
             ((3 as f32) / (4 as f32)) * p1.0 + ((1 as f32) / (4 as f32)) * p2.0,
             ((3 as f32) / (4 as f32)) * p1.1 + ((1 as f32) / (4 as f32)) * p2.1
@@ -28,27 +38,13 @@ pub fn Chaikin_iteration(points: Vec<(Point, Point)>) -> Vec<Point> {
         );
         lines_iteration.push(point_q);
         lines_iteration.push(point_r);
-    }
-    lines_iteration
-}
 
-
-
-// it does not work OMG we wan't do this this way
-pub fn create_animation(steps: usize, mut original_lines: Vec<(Point, Point)>) {
-    let mut current_lines = original_lines;
-    let mut current_points = Vec::new();
-    let mut i = 1;
-
-    while i <= steps {
-        println!("step :{:?}", i);
-
-        thread::sleep(Duration::from_millis(1000));
-
-        current_points = Chaikin_iteration(current_lines.to_vec());
-       
-        current_lines = draw_lines_points(current_points);
-        original_lines = current_lines.clone();
+        if i == points.len() - 1 {
+            let (_, end) = points[points.len() - 1];
+            lines_iteration.push(end);
+        }
         i += 1;
     }
+
+    lines_iteration
 }
